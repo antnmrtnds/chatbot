8956f226a6eeeeff5adb05b41c00d0604c40e362
Antonio Martinho Dias <130603581+antnmrtnds@users.noreply.github.com>
2025-07-02 09:58:29 +0100

    remove snippet

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,10 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <script id="userled-sdk-snippet" data-cfasync="false">
-  window.userledSettings={app_id:"881e48e0-291e-48c6-95f4-9e1fb069a7fd",region:"eu-west-2"},window.userledSnippetTs=(new Date).getTime(),(function(){if(!window.Userled){window.Userled=function(){return e.call(arguments)};var e=window.Userled;e.call=function(n){return new Promise((function(i,w){e.queue.push([].concat.apply([i,w],n))}))},e.queue=[],e.snippetVersion="4.0.0",window.Userled("page")}})();
-</script>
-<script id="userled-sdk" type="module" src="https://sdk.userledclient.io?appId=881e48e0-291e-48c6-95f4-9e1fb069a7fd&snippetVersion=4.0.0" data-cfasync="false"></script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
