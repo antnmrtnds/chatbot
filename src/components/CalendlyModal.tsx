b92c67bce0ba9f073d232cda73598a666cd822a3
Antonio Martinho Dias <130603581+antnmrtnds@users.noreply.github.com>
2025-07-23 14:12:18 +0100

    feat: create CalendlyModal component

---
+++ src/components/CalendlyModal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+interface CalendlyModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  url: string;
+}
+
+export default function CalendlyModal({ isOpen, onClose, url }: CalendlyModalProps) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="w-[95vw] h-[90vh] md:w-[80vw] md:h-[90vh] p-0">
+        <DialogHeader className="p-4 border-b">
+          <DialogTitle>Agende a sua visita</DialogTitle>
+        </DialogHeader>
+        <div className="flex-1 w-full h-full">
+          <iframe
+            src={url}
+            width="100%"
+            height="100%"
+            frameBorder="0"
+          ></iframe>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+} 
